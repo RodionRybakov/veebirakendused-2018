2dd68f1f8d3bcd7f216f8cebc852d3eed466d0f4
Rodion <Rodion.Robakov@gmail.com>
2018-02-21 11:27:43 +0200

    project initialization

---
+++ src/test/java/ee/ut/cs/wad/AdBoard/AdBoardApplicationTests.java
@@ -0,0 +1,16 @@
+package ee.ut.cs.wad.AdBoard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdBoardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
