3034efc95642240af10546519bd05b0917f31121
Rodion <Rodion.Robakov@gmail.com>
2018-04-16 21:03:41 +0300

    multi-language support. closes #16

--- src/main/resources/static/js/account.js
+++ src/main/resources/static/js/account.js
@@ -10,47 +10,45 @@ $(function () {
 					url: '/offers/get',
 					data: 'offerId=' + offerId,
 					success: [
-
 						function (data) {
 							var json = $.parseJSON(data);
 							$('#offerPreview').empty().append(
-								'<div class="col-md-12">\n' +
-								'\t<div class="col-md-3 crane-image-block ">\n' +
-								'\t\t<div class="crane-list-img">\n' +
-								'\t\t\t<img class="img-responsive" src="/img/offer_unknown.png"/>\n' +
-								'\t\t</div>\n' +
-								'\t</div>\n' +
-								'\t<div class="col-md-7">\n' +
-								'\t\t<div class="crane-list-content">\n' +
-								'\t\t\t<div class="crane-title">\n' +
-								'\t\t\t\t<h3>' + json['title'] + '</h3>\n' +
-								'\t\t\t</div>\n' +
-								'\t\t\t<div class="crane-phone">\n' +
-								'\t\t\t\t<ul class="list-inline">\n' +
-								'\t\t\t\t\t<li><i class="fa fa-phone"></i>  <span>' + json['phone'] + '</span></li>\n' +
-								'\t\t\t\t</ul>\n' +
-								'\t\t\t</div>\n' +
-								'\t\t\t<div class="crane-address">\n' +
-								'\t\t\t\t<ul class="list-inline">\n' +
-								'\t\t\t\t\t<li><i class="fa fa-map-marker"></i></li>\n' +
-								'\t\t\t\t\t<li>' + json['address'] + '</li>\n' +
-								'\t\t\t\t</ul>\n' +
-								'\t\t\t</div>\n' +
-								'\t\t\t<div class="crane-category">\n' +
-								'\t\t\t\t<!--<ul class="list-inline">-->\n' +
-								'\t\t\t\t\t<!--<li><a href="#">Cybersecurity</a></li>-->\n' +
-								'\t\t\t\t\t<!--<li><a href="#">Crane Towing Service</a></li>-->\n' +
-								'\t\t\t\t\t<!--<li><a href="#">Salary starting from 10 000$</a></li>-->\n' +
-								'\t\t\t\t\t<!--<li><a href="#">More...</a></li>-->\n' +
-								'\t\t\t\t<!--</ul>-->\n' +
-								'\t\t\t\t<p title="description">' + json['description'] + '</p>\n' +
-								'\t\t\t</div>\n' +
-								'\t\t</div>\n' +
-								'\t</div>\n' +
+								'<div class="col-md-12">' +
+								'    <div class="col-md-3 crane-image-block ">' +
+								'        <div class="crane-list-img">' +
+								'            <img class="img-responsive" src="/img/offer_unknown.png"/>' +
+								'        </div>' +
+								'    </div>' +
+								'    <div class="col-md-7">' +
+								'        <div class="crane-list-content">' +
+								'            <div class="crane-title">' +
+								'                <h3>' + json['title'] + '</h3>' +
+								'            </div>' +
+								'            <div class="crane-phone">' +
+								'                <ul class="list-inline">' +
+								'                    <li><i class="fa fa-phone"></i>  <span>' + json['phone'] + '</span></li>' +
+								'                </ul>' +
+								'            </div>' +
+								'            <div class="crane-address">' +
+								'                <ul class="list-inline">' +
+								'                    <li><i class="fa fa-map-marker"></i></li>' +
+								'                    <li>' + json['address'] + '</li>' +
+								'                </ul>' +
+								'            </div>' +
+								'            <div class="crane-category">' +
+								'                <!--<ul class="list-inline">-->' +
+								'                    <!--<li><a href="#">Cybersecurity</a></li>-->' +
+								'                    <!--<li><a href="#">Crane Towing Service</a></li>-->' +
+								'                    <!--<li><a href="#">Salary starting from 10 000$</a></li>-->' +
+								'                    <!--<li><a href="#">More...</a></li>-->' +
+								'                <!--</ul>-->' +
+								'                <p title="description">' + json['description'] + '</p>' +
+								'            </div>' +
+								'        </div>' +
+								'    </div>' +
 								'</div>'
 							);
 						}
-
 					]
 				});
 				break;
