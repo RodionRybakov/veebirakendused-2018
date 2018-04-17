f1857fe8bc47db8a722a9e9a5b76c36b10d1062f
Rodion <Rodion.Robakov@gmail.com>
2018-04-17 03:37:15 +0300

    cached files offline availability. closes #19

--- src/main/java/ee/ut/cs/wad/AdBoard/application/config/WebMvcConfig.java
+++ src/main/java/ee/ut/cs/wad/AdBoard/application/config/WebMvcConfig.java
@@ -2,16 +2,31 @@ package ee.ut.cs.wad.AdBoard.application.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.resource.CssLinkResourceTransformer;
+import org.springframework.web.servlet.resource.VersionResourceResolver;
 
 import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**")
+				.addResourceLocations("/resources/", "classpath:/other-resources/")
+				.setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS))
+				.resourceChain(false)
+				.addResolver(new VersionResourceResolver().addContentVersionStrategy("/**"))
+				.addTransformer(new CssLinkResourceTransformer());
+	}
 	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
